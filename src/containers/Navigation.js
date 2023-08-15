2fa3e09fa143e4616c5c2e8d9e8206cca6cec960
Andrii Lishchyna <andrej044@i.ua>
2023-08-15 12:25:16 -0400

    create Navigation component

---
+++ src/containers/Navigation.js
@@ -0,0 +1,31 @@
+import GitHub from "../components/socialMedia/gitHub";
+import FrontEndMentor from "../components/socialMedia/frontEndMentor";
+import LinkedIn from "../components/socialMedia/linkedIn";
+
+
+const Navigation = ({section}) => {
+  return(
+    <nav className={`${section}-nav`}>
+      <p>andrej044</p>
+      <ul className="nav__list">
+        <li>
+          <a className="nav__link" href="/some/valid/uri">
+              <GitHub/>
+          </a>
+        </li>
+        <li>
+          <a className="nav__link" href="/some/valid/uri">
+              <FrontEndMentor/>
+          </a>
+        </li>
+        <li>
+          <a className="nav__link" href="/some/valid/uri">
+              <LinkedIn/>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
