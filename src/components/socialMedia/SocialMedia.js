f394b18726bb16717d6283a40752b868a67c9f10
Andrii Lishchyna <andrej044@i.ua>
2023-08-15 12:11:49 -0400

    create experemental component

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -1,21 +1,24 @@
 import { createElement } from "react"
 
+// This is experemental feature, create svg icon element with title and parametrs
+// work in progress
 
 const SocialMedia =({social})=>{
 
   console.log(social)
   const socialMedia = social.map( (item, index) => {
-    // item.content instanceof String ? item.content : item.content;
+
+    
    return createElement(
     item.tag, 
     {className:item.className, key:index}, 
      typeof item.content === "string" ? item.content : createElement(
       item.content.tag,
       {
-        className:item.content.className,
-        fill:item.content.fill,
-        fillRule:item.content.fillRule,
-        d:item.content.d
+        className: typeof item.content.className === "string" ? item.content.className : null ,
+        fill: typeof item.content.fill === "string" ? item.content.fill : null,
+        fillRule: typeof item.content.fillRule === "string" ? item.content.fillRule : null ,
+        d: typeof item.content.d === "string" ? item.content.d : null
       },
     )    
     )
