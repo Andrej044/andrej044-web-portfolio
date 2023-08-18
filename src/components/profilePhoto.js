3dc8440b4eaf6d6dcfc4a70218b1f6c12e72a63b
Andrii Lishchyna <andrej044@i.ua>
2023-08-18 12:44:53 -0400

    add styles for ProfilePhoto component

--- src/components/profilePhoto.js
+++ src/components/profilePhoto.js
@@ -1,10 +1,20 @@
 
 const ProfilePhoto = () => {
 
+  const imgStyle = {  
+      order: 1,
+      width: '100%',
+      height: '367px',
+      background: 'url("../images/profilePhoto.jpg") no-repeat',
+      backgroundPosition: 'center',
+      backgroundSize: 'contain',
+      zIndex: 1
+    }
+
   return(
-    <div className="intro__img">
-    <h3 className="img__title visually-hidden">Profile Photo</h3>
-  </div>
+    <div className={imgStyle}>
+      <h3 className="visually-hidden">Profile Photo</h3>
+    </div>
   )
 }
 
