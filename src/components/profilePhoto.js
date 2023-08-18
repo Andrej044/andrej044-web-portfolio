f4c398504bf69f522288261c13cae210cffd97b7
Andrii Lishchyna <andrej044@i.ua>
2023-08-18 12:12:09 -0400

    create ProfilePhoto component

---
+++ src/components/profilePhoto.js
@@ -0,0 +1,11 @@
+
+const ProfilePhoto = () => {
+
+  return(
+    <div className="intro__img">
+    <h3 className="img__title visually-hidden">Profile Photo</h3>
+  </div>
+  )
+}
+
+export default ProfilePhoto;
