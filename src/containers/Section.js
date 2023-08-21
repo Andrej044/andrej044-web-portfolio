980334aed06a53e7be83e15ece479a3b1f10103e
Andrii Lishchyna <andrej044@i.ua>
2023-08-21 13:18:45 -0400

    create Section component

---
+++ src/containers/Section.js
@@ -0,0 +1,19 @@
+import { createElement} from "react";
+import React from "react";
+
+ const Section = ({name, children,hidden}) => {
+const titleContent = name.charAt(0).toUpperCase() + name.slice(1);
+const childrenElements = children.map((elem, index) => {
+  console.log(elem)
+  return createElement(elem.type, {className:elem.props.className, key:index}, elem.props.children);
+}) 
+return(
+  <section className={name}>
+    <h2 className={`${name}__title ${hidden ? 'visually-hidden' : ''}`}>{titleContent}</h2>
+    {childrenElements}
+  </section>
+)
+
+ }
+
+ export default Section
