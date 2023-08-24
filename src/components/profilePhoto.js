180195200fd16c8124ade58f8e2768766aa3cc7a
Andrii Lishchyna <andrej044@i.ua>
2023-08-24 12:03:53 -0400

    upd component

--- src/components/profilePhoto.js
+++ src/components/profilePhoto.js
@@ -1,24 +1,8 @@
-
 import background from '../images/profilePhoto.jpg'
 
-const ProfilePhoto = () => {
-
-  const imgStyle = {  
-      order: 1,
-      width: '320px',
-      height: '367px',
-      backgroundImage: `url(${background})`,
-      backgroundRepeat:'no-repeat',
-      backgroundPosition: 'center 25%',
-      backgroundSize: 'cover',
-      border: '2px solid white',
-      borderRadius: '50%',
-      zIndex: 1,
-      filter: 'grayscale(100%) sepia(41%)'
-    }
-
+const ProfilePhoto = ({props}) => {
   return(
-    <div style={imgStyle}>
+    <div style={{backgroundImage: `url(${background})`}} className={`${props.name}__img`}>
       <h3 className="visually-hidden">Profile Photo</h3>
     </div>
   )
