9e25eecea59c8d5019936a50a46c7c9a10566156
Andrii Lishchyna <andrej044@i.ua>
2023-08-26 00:26:05 -0400

    upd component

--- src/components/projectLinksComponent.js
+++ src/components/projectLinksComponent.js
@@ -1,7 +1,7 @@
 const ProjectsLinkComponent = ({projectLink, projectCode}) => {
   return (
     <>
-      <a className="btn" href={projectLink}>view projects</a>
+      <a className="btn" href={projectLink}>view project</a>
       <a className="btn" href={projectCode}>view code</a>
     </>
   )
