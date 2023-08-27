efbdf79bb8422eba89b504c63134129fcd9c4719
Andrii Lishchyna <andrej044@i.ua>
2023-08-26 20:49:06 -0400

    change star size

--- src/components/svg/star.js
+++ src/components/svg/star.js
@@ -2,7 +2,7 @@
 const Star = ({color, strokeColor = "#1C274C"}) => {
 
   return (
-    <svg viewBox="0 0 24 24" width="129" height="129" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 24 24" width="50" height="50" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
       <g id="SVGRepo_iconCarrier"> 
