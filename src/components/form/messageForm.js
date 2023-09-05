553cb70b6a168ec2ad0d789188e3b1f8124b2870
Andrii Lishchyna <andrej044@i.ua>
2023-09-05 19:39:23 -0400

    implement useForm hook

--- src/components/form/messageForm.js
+++ src/components/form/messageForm.js
@@ -10,9 +10,9 @@ const MessageForm = () => {
     console.log(data)
   }
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  // const [name, setName] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [message, setMessage] = useState('');
 
   // const handleSubmit =(e)=>{
   //   e.preventDefault();
@@ -48,9 +48,16 @@ const MessageForm = () => {
         </div>
         <div className="form__input-wrapper input-email">
           <input 
+            {... register('email', {
+            required:true,
+            // pattern:{
+            //   value:/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+            //   message:<p>alert message</p>
+            //   }
+            })}
             className="form__input"
-            value={email}
-            onChange={e => setEmail(e.target.value)} 
+            // value={email}
+            // onChange={e => setEmail(e.target.value)} 
             id="email" 
             name="email" 
             type="email" 
@@ -60,14 +67,16 @@ const MessageForm = () => {
         </div>
         <div className="form__input-wrapper input-message">
           <textarea 
+            {...register('message', {
+              required:true
+            })}
             className="form__input"
-            value={message}
-            onChange={e => setMessage(e.target.value)} 
+            // value={message}
+            // onChange={e => setMessage(e.target.value)} 
             id="message" 
             name="message" 
             placeholder="message" 
-            spellCheck="false" 
-            required></textarea>
+            spellCheck="false" ></textarea>
         </div>
         <button id="submit" className="btn form__btn-submit" type="submit">
           send message
