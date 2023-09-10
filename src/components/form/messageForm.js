4bc4b36503f384b18dc7812fbd6bb2d537f84e52
Andrii Lishchyna <andrej044@i.ua>
2023-09-10 19:19:22 -0400

    implement useForm hook, add validation and error handler

--- src/components/form/messageForm.js
+++ src/components/form/messageForm.js
@@ -1,83 +1,80 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import Input from './input';
 import './messageForm.css';
 
 const MessageForm = () => {
 
-  const {register, handleSubmit} = useForm();
+  const {register, handleSubmit, formState:{errors}} = useForm();
 
   const onSubmit = (data) => {
-    console.log(data)
+    console.log(data);
   }
-
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [message, setMessage] = useState('');
-
-  // const handleSubmit =(e)=>{
-  //   e.preventDefault();
-    
-  //   const form = e.target;
-  //   const formData = new FormData(form);
-
-  //   const formJson = Object.fromEntries(formData.entries());
-  //   console.log(formJson)
-  // }
 
   return(
     <form id="myForm" method='post' onSubmit={handleSubmit(onSubmit)}>
       <div className="contact-form">
         <div className="form__input-wrapper">  
           <input 
-            {...register('name', {
-              required:true, 
-              minLength:6,
-              maxLength:30,
-              pattern:/^[A-Za-z]+$/i,
-              })}
+            {...register('firstName', {
+              required:"Field is required", 
+              minLength:{
+                value:5,
+                message:"Minimum name length 5 characters"
+              },
+              maxLength:{
+                value:30,
+                message:"Minimum name length 30 characters"
+              },
+              pattern:{
+                value:/^[A-Za-z]+$/i,
+                message: 'Wrong format'
+                }}
+                )}
             className="form__input" 
-            // value={name}
-            // onChange={e=>setName(e.target.value)} 
-            id="name" 
-            name="name" 
             type="text" 
             placeholder="name" 
             autoComplete="name" 
-            // required
             />
+          <span className="email-alert">{errors?.firstName && (errors?.firstName?.message || 'Error!')}</span>
         </div>
         <div className="form__input-wrapper input-email">
           <input 
-            {... register('email', {
-            required:true,
-            // pattern:{
-            //   value:/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-            //   message:<p>alert message</p>
-            //   }
+            {...register('email', {
+            required:"Field is required",
+            pattern:{
+              value:/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+              message:'Please enter valid email'
+              }
             })}
             className="form__input"
-            // value={email}
-            // onChange={e => setEmail(e.target.value)} 
-            id="email" 
-            name="email" 
             type="email" 
             placeholder="email" 
             autoComplete="off" />
-          <span className="email-alert hidden">Sorry, invalid format here</span>
+          <span className="email-alert">{errors?.email && (errors?.email?.message || 'Error!')}</span>
         </div>
         <div className="form__input-wrapper input-message">
           <textarea 
-            {...register('message', {
-              required:true
+            {...register('textMessage', {
+              required:"Field is required",
+              minLength:{
+                value:25,
+                message:"Could you kindly provide me with a sentence that consists of at least 25 characters?"
+              }
             })}
             className="form__input"
-            // value={message}
-            // onChange={e => setMessage(e.target.value)} 
-            id="message" 
-            name="message" 
-            placeholder="message" 
+            placeholder="Could you kindly provide me with a sentence that consists of at least 25 characters?" 
             spellCheck="false" ></textarea>
+          <span className="email-alert">{errors?.textMessage && (errors?.textMessage?.message || 'Error!')}</span>
         </div>
+        {/* <div className="form__input-wrapper">
+          <Input {...register('password', {
+            required:{
+              value:true,
+              message: 'this field is required'
+            }
+          })} name="password" placeholder="Enter password" className="form__input" errors= {errors}/>
+        </div> */}
         <button id="submit" className="btn form__btn-submit" type="submit">
           send message
         </button>
