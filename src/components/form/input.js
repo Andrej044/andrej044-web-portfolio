8f99e88051f04a081045a712f9ad316d9b936041
Andrii Lishchyna <andrej044@i.ua>
2023-09-11 19:30:32 -0400

    update Input component

--- src/components/form/input.js
+++ src/components/form/input.js
@@ -1,35 +1,24 @@
-// // import { useForm } from "react-hook-form";
 import { useState} from "react";
-// import React from "react";
 
 const Input = (props) => {
+  const {name, errors, placeholder, className} = props;
+  console.log(errors)
+  
   const [value, setValue] = useState(props.value || "");
-
   return(
-    <input 
-      name={props.name}
-      onChange={(e) => {
-        setValue(e.target.value);
+    <>
+      <input 
+        name={name} 
+        className={className}   
+        value={value} 
+        placeholder={placeholder}       
+        onChange={(e) => {
+          setValue(e.target.value);
         props.onChange && props.onChange(e)
-      }}
-      value={value}
-    />
+        }}/>
+       <span className="email-alert">{errors && (errors?.message || 'Error!')}</span>
+     </>
   )
 }
-
-// const Input = React.forwardRef(function Input(props,ref){
-//   const {name, errors, placeholder, className} = props;
-//   // const err = errors[name];
-//   console.log(errors[name])
-//   return(
-//     <>
-//       <input className={className} placeholder={placeholder} ref={ref}/>
-//       <span className="email-alert">{errors?.[name] && (errors?.[name]?.message || 'Error!')}</span>
-//     </>
-//   )
-// });
-// // const Input = React.forwardRef(({name, placeholder }, ref) => (
-// //   <input name={name} ref={ref} />
-// // ));
 
 export default Input
