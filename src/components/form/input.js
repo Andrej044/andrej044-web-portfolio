bac5440b6fda68fc989781243d62a4f34b22c9b0
Andrii Lishchyna <andrej044@i.ua>
2023-09-11 18:15:37 -0400

    build input component from the scratch

--- src/components/form/input.js
+++ src/components/form/input.js
@@ -1,11 +1,24 @@
-import { useForm } from "react-hook-form"
+// // import { useForm } from "react-hook-form";
+// // import { useState, useEffect } from "react";
+// import React from "react";
 
-const Input = (name, placeholder) => {
-  const {register, formState:{errors}} = useForm();
+// const Input = React.forwardRef(function Input(props,ref){
   
-  return(
-    <input {...register(name)} placeholder={placeholder}/>
-  )
-}
+//   const {name, errors, placeholder, className} = props;
+  
+//   // const err = errors[name];
+//   console.log(errors[name])
+//   return(
+//     <>
+//       <input className={className} placeholder={placeholder} ref={ref}/>
+//       <span className="email-alert">{errors?.[name] && (errors?.[name]?.message || 'Error!')}</span>
+//     </>
+//   )
+// });
 
-export default Input
+
+// // const Input = React.forwardRef(({name, placeholder }, ref) => (
+// //   <input name={name} ref={ref} />
+// // ));
+
+// export default Input
