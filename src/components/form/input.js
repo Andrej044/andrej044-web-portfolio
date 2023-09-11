1e2965056b22a8786cdeb70013d949099549ef84
Andrii Lishchyna <andrej044@i.ua>
2023-09-11 18:23:41 -0400

    define basic Input component

--- src/components/form/input.js
+++ src/components/form/input.js
@@ -1,11 +1,24 @@
 // // import { useForm } from "react-hook-form";
-// // import { useState, useEffect } from "react";
+import { useState} from "react";
 // import React from "react";
 
+const Input = (props) => {
+  const [value, setValue] = useState(props.value || "");
+
+  return(
+    <input 
+      name={props.name}
+      onChange={(e) => {
+        setValue(e.target.value);
+        props.onChange && props.onChange(e)
+      }}
+      value={value}
+    />
+  )
+}
+
 // const Input = React.forwardRef(function Input(props,ref){
-  
 //   const {name, errors, placeholder, className} = props;
-  
 //   // const err = errors[name];
 //   console.log(errors[name])
 //   return(
@@ -15,10 +28,8 @@
 //     </>
 //   )
 // });
-
-
 // // const Input = React.forwardRef(({name, placeholder }, ref) => (
 // //   <input name={name} ref={ref} />
 // // ));
 
-// export default Input
+export default Input
