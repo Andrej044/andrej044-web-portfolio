c5e4d1a5f0456318593339e125dacc51af61fe4f
Andrii Lishchyna <andrej044@i.ua>
2023-09-11 20:03:56 -0400

    upd Input component, now we can define form field in contact form

--- src/components/form/input.js
+++ src/components/form/input.js
@@ -1,20 +1,25 @@
-import { useState} from "react";
+import { createElement, useState} from "react";
 
 const Input = (props) => {
-  const {name, errors, placeholder, className, onChange} = props;
+  const {name, type, errors, placeholder, className, onChange} = props;
   
   const [value, setValue] = useState(props.value || "");
+  
+  const inputType = createElement(
+      type, 
+      {name:name, 
+      className:className,   
+      value:value, 
+      placeholder:placeholder,       
+      onChange:(e) => {
+      setValue(e.target.value);
+      onChange && onChange(e)
+    }
+  })
+  
   return(
     <>
-      <input 
-        name={name} 
-        className={className}   
-        value={value} 
-        placeholder={placeholder}       
-        onChange={(e) => {
-          setValue(e.target.value);
-          onChange && onChange(e)
-        }}/>
+      {inputType}
        <span className="email-alert">{errors && (errors?.message || 'Error!')}</span>
      </>
   )
