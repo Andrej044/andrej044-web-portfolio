315be94582d25306d4b534d457a06b5a69cb4343
Andrii Lishchyna <andrej044@i.ua>
2023-09-14 18:41:31 -0400

    create default form, useForm hook with reset

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -5,10 +5,24 @@ import './messageForm.css';
 
 
 const Form = () => {
+  const {register, handleSubmit, reset, formState, formState:{isSubmitSuccesful}} = useForm ({
+    defaultValues : {
+      Name:"",
+      Email:"",
+      Message:""
+  }})
+  
+ const onSubmit = data => {
+  console.log(data)
+  
+ }
 
   return(
     <form>
-
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("something")} />
+      <input type="submit" />
+    </form>
     </form>
   )
 }
