c37d92944b07850d2e3772e672a732ebd142490d
Andrii Lishchyna <andrej044@i.ua>
2023-09-14 18:43:50 -0400

    import emailJs library, implement functional

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,10 +1,13 @@
+import React, {useRef, useEffect} from 'react';
 import { useForm} from 'react-hook-form';
+import emailjs from '@emailjs/browser';
 import './messageForm.css';
 
 
 
 
 const Form = () => {
+  const form = useRef();
   const {register, handleSubmit, reset, formState, formState:{isSubmitSuccesful}} = useForm ({
     defaultValues : {
       Name:"",
@@ -14,12 +17,18 @@ const Form = () => {
   
  const onSubmit = data => {
   console.log(data)
-  
+  emailjs.sendForm('service_7u9iss8', 'template_yxie3y1', form.current, 'pdBeFRcrGLSWWeybO')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+
  }
 
   return(
     <form>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} ref={form}>
       <input {...register("something")} />
       <input type="submit" />
     </form>
