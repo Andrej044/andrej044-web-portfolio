119c1fc879fa3f056a296449418816f3a50dda70
Andrii Lishchyna <andrej044@i.ua>
2023-09-17 14:10:46 -0400

    Create a div that reports the status of sending a message

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,7 +1,30 @@
-import {useRef, useEffect} from 'react';
+import {useRef, useEffect, useState} from 'react';
 import { useForm} from 'react-hook-form';
 import emailjs from '@emailjs/browser';
 import './messageForm.css';
+
+
+const PopUpWindow = (props) => {
+  const style ={
+    width: '50vw',
+    height:'15vw',
+    position:'absolute',
+    top:'0',
+    left:'0',
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'space-between',
+    transform: 'translateX(50%)',
+    backgroundColor: 'rgba(50,50,50,0.5)'
+
+  }
+  return(
+    <div style={style}>
+      <p>{props.message}</p>
+      <button style={{width: '100%', fontSize:'24px', backgroundColor:'rgba(150,150,150,0.8)', borderRadius:'5px'}} type='button'>Ok</button>
+    </div>
+  )
+}
 
 const Form = () => {
   const {register, handleSubmit, reset, formState} = useForm ({
@@ -11,16 +34,25 @@ const Form = () => {
       user_message:""
   }})
 
-  
+  const[message, setMessage] = useState('');
+
   const form = useRef();
   
+  const RenderMessage = (props) => {
+    return (
+      <PopUpWindow message = {props.message}/>
+    )
+  } 
+
   const onSubmit = (data) => {
   console.log(data)
   emailjs.sendForm('service_7u9iss8', 'template_yxie3y1', form.current, 'pdBeFRcrGLSWWeybO')
     .then((result) => {
-        console.log(result.text);
+      console.log(result.text);
+      setMessage("Awesome! Your message has been sent to Andrii and he'll be so happy to hear from you! Thank you for your message!");
     }, (error) => {
         console.log(error.text);
+        setMessage("Oops! We're sorry, something seems to have gone wrong. Don't worry, we'll get it fixed soon! Please try again later.");
     });
 
  }
@@ -35,7 +67,11 @@ const Form = () => {
   }
  }, [formState, reset])
 
+
+
+
   return(
+    <>
     <form id="myForm" method='post' onSubmit={handleSubmit(onSubmit)} ref={form}>
       <div className="contact-form">
         <div className="form__input-wrapper">
@@ -80,6 +116,8 @@ const Form = () => {
         <input id="submit" className="btn form__btn-submit" type="submit" value={'send message'}/>
       </div>
     </form>
+    <RenderMessage message = {message} />
+    </>
   )
 }
 
