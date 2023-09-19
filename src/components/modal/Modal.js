01d6b6664aebfaf675714303e8cc6afc5dbff987
Andrii Lishchyna <andrej044@i.ua>
2023-09-19 15:18:53 -0400

    add render message which coming from props

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,10 +1,11 @@
 import './Modal.css'
 
 
-const Modal = ({active, setActive}) => {
+const Modal = ({active, setActive, message}) => {
   return(
     <div className={active ? 'modal active': 'modal'}>
       <div className='modal__content'>
+        <p>{message}</p>
         <button className='btn modal__btn' onClick={()=>setActive(false)}>OK</button>
       </div>
     </div>
