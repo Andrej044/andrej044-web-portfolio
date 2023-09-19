5a505e4f6406d09736271c9c822b1b846e9f1ca5
Andrii Lishchyna <andrej044@i.ua>
2023-09-19 15:10:05 -0400

    add button for change modal active state, add styles for button

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -5,7 +5,7 @@ const Modal = ({active, setActive}) => {
   return(
     <div className="modal">
       <div className='modal__content'>
-        
+        <button className='btn modal__btn' onClick={()=>setActive(false)}>OK</button>
       </div>
     </div>
   )
