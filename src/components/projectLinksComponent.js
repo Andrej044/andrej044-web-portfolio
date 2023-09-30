e71a54a9351fd22afee6dacb04cfa99f2b19e62b
Andrii Lishchyna <andrej044@i.ua>
2023-09-30 15:51:50 -0400

    add target="_blank" attribute

--- src/components/projectLinksComponent.js
+++ src/components/projectLinksComponent.js
@@ -1,8 +1,8 @@
 const ProjectsLinkComponent = ({projectLink, projectCode}) => {
   return (
     <div className="project__links-wrapper">
-      <a className="btn" href={projectLink}>view project</a>
-      <a className="btn" href={projectCode}>view code</a>
+      <a className="btn" href={projectLink} rel='noreferrer' target="_blank">view project</a>
+      <a className="btn" href={projectCode} rel='noreferrer' target="_blank">view code</a>
     </div>
   )
 }
