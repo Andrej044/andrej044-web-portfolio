6355c7322d7a675a53260ab01b3081d7b2cae3a2
Andrii Lishchyna <andrej044@i.ua>
2023-10-01 13:47:20 -0400

    add describe about me. Clean code

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -28,7 +28,7 @@ const Main = () =>{
       </span>
       <div className="intro__text">
         <p>Nice to <br className="intro__text--br-hidden"/> meet you! I'm <span className="text--underline">Andrii Lishchyna</span>.</p>
-        <p>Based in the Canada, Toronto,  and I'm a front-end developer passionate about building accessible web apps that users love.</p>
+        <p>As a front-end developer and troubleshooter, I possess the skills and expertise to develop user-friendly interfaces and diagnose and resolve technical issues efficiently.</p>
         <a className="btn intro__link" href="#contact">Contact Me</a>
       </div>
       <ProfilePhoto name='intro'/>
@@ -48,98 +48,6 @@ const Main = () =>{
         <a className="btn projects__link" href="#contact">Contact Me</a>
       </div>
       <ProjectsList/>
-      {/* <ul className="projects__list">
-        <li className="projects__item">
-          <img className="projects__preview-img" src="images/thumbnail-project-1-large.webp" alt="design portfolio" />
-          <div className="projects__describe">
-            <h3 className="describe__title">design portfolio</h3>
-            <ul className="projects__tech-list">
-              <li className="describe__item">html</li>
-              <li className="describe__item">css</li>
-            </ul>
-            <div className="describe__links-wrapper">
-              <a className="btn" href="/some/valid/uri">view projects</a>
-              <a className="btn" href="/some/valid/uri">view code</a>
-            </div>
-          </div>
-        </li>
-        <li className="projects__item">
-          <img className="projects__preview-img" src="images/thumbnail-project-2-large.webp" alt="e-learning landing page" />
-          <div className="projects__describe">
-            <h3 className="describe__title">e-learning landing page</h3>
-            <ul className="projects__tech-list">
-              <li className="describe__item">html</li>
-              <li className="describe__item">css</li>
-            </ul>
-            <div className="describe__links-wrapper">
-              <a className="btn" href="/some/valid/uri">view projects</a>
-              <a className="btn" href="/some/valid/uri">view code</a>
-            </div>
-          </div>
-        </li>
-        <li className="projects__item">
-          <img className="projects__preview-img" src="images/thumbnail-project-3-large.webp" alt="todo web app" />
-          <div className="projects__describe">
-            <h3 className="describe__title">todo web app</h3>
-            <ul className="projects__tech-list">
-              <li className="describe__item">html</li>
-              <li className="describe__item">css</li>
-              <li className="describe__item">javascript</li>
-            </ul>
-            <div className="describe__links-wrapper">
-              <a className="btn" href="/some/valid/uri">view projects</a>
-              <a className="btn" href="/some/valid/uri">view code</a>
-            </div>
-          </div>
-        </li>
-        <li className="projects__item">
-          <img className="projects__preview-img" src="images/thumbnail-project-4-large.webp" alt="entertainment web app" />
-          <div className="projects__describe">
-            <h3 className="describe__title">entertainment web app</h3>
-            <ul className="projects__tech-list">
-              <li className="describe__item">html</li>
-              <li className="describe__item">css</li>
-              <li className="describe__item">javascript</li>
-            </ul>
-            <div className="describe__links-wrapper">
-              <a className="btn" href="/some/valid/uri">view projects</a>
-              <a className="btn" href="/some/valid/uri">view code</a>
-            </div>
-          </div>
-        </li>
-        <li className="projects__item">
-          <img className="projects__preview-img" src="images/thumbnail-project-5-large.webp" alt="memory game" />
-          <div className="projects__describe">
-            <h3 className="describe__title">memory game</h3>
-            <ul className="projects__tech-list">
-              <li className="describe__item">html</li>
-              <li className="describe__item">css</li>
-              <li className="describe__item">javascript</li>
-            </ul>
-            <div className="describe__links-wrapper">
-              <a className="btn" href="/some/valid/uri">view projects</a>
-              <a className="btn" href="/some/valid/uri">view code</a>
-            </div>
-          </div>
-        </li>
-        <li className="projects__item">
-          <div className="preview-img--wrapper">
-            <img className="projects__preview-img" src="images/thumbnail-project-6-large.webp" alt="art gallery showcase"/>
-          </div>
-          <div className="projects__describe">
-            <h3 className="describe__title">art gallery showcase</h3>
-            <ul className="projects__tech-list">
-              <li className="describe__item">html</li>
-              <li className="describe__item">css</li>
-              <li className="describe__item">javascript</li>
-            </ul>
-            <div className="describe__links-wrapper">
-              <a className="btn" href="/some/valid/uri">view projects</a>
-              <a className="btn" href="/some/valid/uri">view code</a>
-            </div>
-          </div>
-        </li>
-      </ul> */}
     </Section>
   </main>
   )
